5cc1ca2400adb3ea91376c464f1147f4c3c57699
Jonathan Druet <jonathandruet2@gmail.com>
2023-08-15 18:11:31 +0200

    add form 90% , manque a faire l'appel au service du back end et tester si ca ajoute en db + regarder pour faire l'upload de l'image etc qui reste incertains car jsp

--- API_RocketLeague_Project_FrontEnd/src/app/components/player/add-player/add-player.component.ts
+++ API_RocketLeague_Project_FrontEnd/src/app/components/player/add-player/add-player.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Ranks} from "../../../models/Ranks";
 import {Plateforms} from "../../../models/Plateforms";
+import {Divisions} from "../../../models/Divisions";
+import {Rank} from "../../../models/Rank";
 
 @Component({
   selector: 'app-add-player',
@@ -10,25 +12,46 @@ import {Plateforms} from "../../../models/Plateforms";
 })
 export class AddPlayerComponent {
   playerForm : FormGroup;
-  ranks: string[] = Object.keys(Ranks).filter(key => isNaN(Number(key)));
+  ranksEnum: string[] = Object.keys(Ranks).filter(key => isNaN(Number(key)));
+  divisions: string[] = Object.keys(Divisions).filter(key => isNaN(Number(key)));
   plateforms: string[] = Object.keys(Plateforms).filter(key => isNaN(Number(key)));
+  ranks: Rank[] = []; // Tableau pour stocker les informations des rangs et divisions combinées
   constructor(private _fb : FormBuilder) {
     this.playerForm = this._fb.group({
       pseudo:[null,[Validators.required]],
-      ranks:[null,[Validators.required]],
-      plateform:[null,[Validators.required]],
-      image:[null,[Validators.required,Validators.min(0)]],
-      wins:[null,[Validators.required,Validators.min(0)]],
-      goals:[null,[Validators.required,Validators.min(0)]],
-      assists:[null,[Validators.required,Validators.min(0)]],
-      mvps:[null,[Validators.required,Validators.min(0)]],
-      shots:[null,[Validators.required,Validators.min(0)]],
-      saves:[null,[Validators.required,Validators.min(0)]]
+      ranks:this._fb.array([
+        this._fb.group({
+          id:0,
+          name:"1s",
+          rank: ['UNRANKED', Validators.required],
+          division: ['UNRANKED', Validators.required]
+        }),
+        this._fb.group({
+          id:0,
+          name:"2s",
+          rank: ['UNRANKED', Validators.required],
+          division: ['UNRANKED', Validators.required]
+        }),
+        this._fb.group({
+          id:0,
+          name:"3s",
+          rank: ['UNRANKED', Validators.required],
+          division: ['UNRANKED', Validators.required]
+        })
+      ]),
+      plateform:["PC",[Validators.required]],
+      image:["",[Validators.required,Validators.min(0)]],
+      wins:[0,[Validators.required,Validators.min(0)]],
+      goals:[0,[Validators.required,Validators.min(0)]],
+      assists:[0,[Validators.required,Validators.min(0)]],
+      mvps:[0,[Validators.required,Validators.min(0)]],
+      shots:[0,[Validators.required,Validators.min(0)]],
+      saves:[0,[Validators.required,Validators.min(0)]]
     })
   }
 
   addPlayer(){
-    console.log(this.playerForm)
+    console.log(this.playerForm.value)
   }
 
   protected readonly Ranks = Ranks;
